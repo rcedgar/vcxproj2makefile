14f750604dc59bbf460efa7825f034f002c49ea3
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2021-07-15 22:42:49 +0100

    Code cleanups

--- vcxproj2makefile.py
+++ vcxproj2makefile.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
-import sys
+
 import os
-import re
+import sys
+
 
 def Die(s):
     print("***ERROR***", sys.argv[0], s, file=sys.stderr)
     sys.exit(1)
+
 
 LRT = False
 DEBUG = False
@@ -20,7 +22,7 @@ for FileName in os.listdir("."):
         ProjFileName = FileName
     elif FileName.endswith(".h"):
         HdrNames.append(FileName)
-if ProjFileName == None:
+if ProjFileName is None:
     print("Project file not found in current directory", file=sys.stderr)
 
 Fields = ProjFileName.split("/")
@@ -34,46 +36,45 @@ fo = open("Makefile_osx", "w")
 
 Linux = "Linux"
 OSX = "OSX"
-def Out(s, OS = "ALL"):
-    global f, fo
+
+
+def Out(s, OS="ALL"):
     if OS == "ALL":
         print(s, file=f)
         print(s, file=fo)
-    elif OS == "Linux":
+    elif OS == Linux:
         print(s, file=f)
-    elif OS == "OSX":
+    elif OS == OSX:
         print(s, file=fo)
     else:
         Die("OS %s", OS)
 
+
 CPPNames = []
 CNames = []
-File = open(ProjFileName)
-while 1:
-    Line = File.readline()
-    if len(Line) == 0:
-        break
-    Line = Line.strip()
-    Line = Line.replace('"', '')
-    Line = Line.replace(' ', '')
-    # <ClCompile Include="betadiv.cpp" />
-    if Line.startswith("<ClCompileInclude"):
-        Fields = Line.split("=")
-        if len(Fields) != 2:
-            continue
-        FileName = Fields[1]
-        FileName = FileName.replace("/>", "")
-        if FileName.endswith(".cpp"):
-            FileName = FileName.replace(".cpp", "")
-            CPPNames.append(FileName)
-        elif FileName.endswith(".c"):
-            FileName = FileName.replace(".c", "")
-            CNames.append(FileName)
-    #     <ProjectName>usearch</ProjectName>
-    elif Line.startswith("<ProjectName"):
-        Line = Line.replace("<ProjectName>", "")
-        Line = Line.replace("</ProjectName>", "")
-        progname = Line
+with open(ProjFileName) as File:
+    for Line in File:
+        Line = Line.strip()
+        Line = Line.replace('"', '')
+        Line = Line.replace(' ', '')
+        # <ClCompile Include="betadiv.cpp" />
+        if Line.startswith("<ClCompileInclude"):
+            Fields = Line.split("=")
+            if len(Fields) != 2:
+                continue
+            FileName = Fields[1]
+            FileName = FileName.replace("/>", "")
+            if FileName.endswith(".cpp"):
+                FileName = FileName.replace(".cpp", "")
+                CPPNames.append(FileName)
+            elif FileName.endswith(".c"):
+                FileName = FileName.replace(".c", "")
+                CNames.append(FileName)
+        #     <ProjectName>usearch</ProjectName>
+        elif Line.startswith("<ProjectName"):
+            Line = Line.replace("<ProjectName>", "")
+            Line = Line.replace("</ProjectName>", "")
+            progname = Line
 
 assert len(CPPNames) > 0 or len(CNames) > 0
 
@@ -81,14 +82,10 @@ binname = progname
 if DEBUG:
     binname += "d"
 
-if binname.find("/") > 0:
-    Fields = binname.split("/")
-    N = len(Fields)
-    binname = Fields[N-1]
-if binname.find("\\") > 0:
-    Fields = binname.split("\\")
-    N = len(Fields)
-    binname = Fields[N-1]
+if "/" in binname:
+    binname = binname.split("/")[-1]
+if "\\" in binname:
+    binname = binname.split("\\")[-1]
 
 # CNames CPPNames
 
@@ -108,21 +105,15 @@ Out("LNKOPTS = -O3 -fopenmp -pthread -lpthread", OSX)
 
 Out("")
 Out("HDRS = \\")
-n = len(HdrNames)
-for i in range(0, n):
-    Name = HdrNames[i]
+for Name in sorted(HdrNames):
     Out("  %s \\" % Name)
 
 Out("")
 Out("OBJS = \\")
-n = len(CPPNames)
-for i in range(0, n):
-    Name = CPPNames[i]
+for Name in CPPNames:
     Out("  o/%s.o \\" % Name)
 
-n = len(CNames)
-for i in range(0, n):
-    Name = CNames[i]
+for Name in CNames:
     Out("  o/%s.o \\" % Name)
 
 Out("")
@@ -146,3 +137,6 @@ for Name in CPPNames:
     Out("")
     Out("o/%s.o : %s.cpp $(HDRS)" % (Name, Name))
     Out("	$(CPP) $(CPPOPTS) -o o/%s.o %s.cpp" % (Name, Name))
+
+f.close()
+fo.close()
